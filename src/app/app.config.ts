f89172b2bf99c26df87481973bb4906ecb3ae2b4
bkedwards <bkedwar@unc.edu>
2025-05-05 16:11:42 +0000

    created website, working on resume page

---
+++ src/app/app.config.ts
@@ -0,0 +1,15 @@
+import {provideHttpClient} from '@angular/common/http';
+import {ApplicationConfig} from '@angular/core';
+import {provideClientHydration} from '@angular/platform-browser';
+import {provideRouter, withHashLocation} from '@angular/router';
+
+import {routes} from './app.routes';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideRouter(
+        routes, withHashLocation()),  // Keep withHashLocation for GitHub Pages
+    provideClientHydration(),
+    provideHttpClient(),
+  ],
+};
---
+++ src/app/app.routes.ts
@@ -0,0 +1,14 @@
+import { Routes } from '@angular/router';
+
+import { AboutComponent } from './about/about.component';
+import { ExperiencesComponent } from './experiences/experiences.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ResumeComponent } from './resume/resume.component';
+
+export const routes: Routes = [
+  { path: '', redirectTo: 'about', pathMatch: 'full' },
+  { path: 'about', component: AboutComponent },
+  {path: 'experiences', component: ExperiencesComponent},
+  {path: 'projects', component: ProjectsComponent},
+  {path: 'resume', component: ResumeComponent}
+];
