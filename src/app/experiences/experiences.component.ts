a95b8390cfc6fbfbe20c67d1e86cc228365fb773
bkedwards <bkedwar@unc.edu>
2025-05-06 01:13:10 +0000

    finished experiences section

--- src/app/experiences/experiences.component.ts
+++ src/app/experiences/experiences.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  AfterViewInit,
+  QueryList,
+  ViewChildren,
+  ElementRef,
+} from '@angular/core';
 
 @Component({
   selector: 'app-experiences',
   imports: [],
   templateUrl: './experiences.component.html',
-  styleUrl: './experiences.component.css'
+  styleUrl: './experiences.component.css',
 })
-export class ExperiencesComponent {
+export class ExperiencesComponent implements AfterViewInit {
+  @ViewChildren('card') cards!: QueryList<ElementRef>;
 
+  animateCards() {
+    this.cards.forEach((cardRef, index) => {
+      setTimeout(() => {
+        cardRef.nativeElement.classList.add('slide-in');
+      }, index * 200); // stagger
+    });
+  }
+
+  ngAfterViewInit(): void {
+    this.animateCards();
+  }
 }
