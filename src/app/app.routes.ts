63d010fc3e7a79645a5a54fbc3ba67cef1105792
bkedwards <bkedwar@unc.edu>
2025-05-17 22:00:15 +0000

    shortened font-size to 12px, removed debug routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,8 @@
 import { Routes } from '@angular/router';
-
-import { AboutComponent } from './about/about.component';
-import { ProjectsComponent } from './projects/projects.component';
-import { SkillsComponent } from './skills/skills.component';
-import { WorkComponent } from './work/work.component';
-import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: 'about', component: AboutComponent },
-  { path: 'projects', component: ProjectsComponent },
-  { path: 'skills', component: SkillsComponent },
-  { path: 'work', component: WorkComponent },
-  { path: 'contact', component: ContactComponent },
   { path: '**', redirectTo: 'home' },
 ];
