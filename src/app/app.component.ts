395008cf715bb66f31119041de51afb1b7d0eb9a
bkedwards <bkedwar@unc.edu>
2026-02-28 21:44:16 +0000

    light mode added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,35 @@
-import {Component} from '@angular/core';
-import {RouterOutlet} from '@angular/router';
-
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, MatIconModule, MatButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'bkedwards4';
+export class AppComponent implements OnInit {
+  isLight = false;
+
+  ngOnInit() {
+    const stored = localStorage.getItem('theme');
+    if (stored === 'light') {
+      this.isLight = true;
+      document.body.classList.add('light-theme');
+    }
+  }
+
+  toggleTheme() {
+    this.isLight = !this.isLight;
+
+    if (this.isLight) {
+      document.body.classList.add('light-theme');
+      localStorage.setItem('theme', 'light');
+    } else {
+      document.body.classList.remove('light-theme');
+      localStorage.setItem('theme', 'dark');
+    }
+  }
 }
